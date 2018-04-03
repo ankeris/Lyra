9f8c5644bc3c91a903aa2dedde57991c8b897670
Juozas Rastenis <aneke1234@gmail.com>
2018-04-03 14:30:45 +0200

    html, css changes

--- public/js/manual/highlightProduct.js
+++ public/js/manual/highlightProduct.js
@@ -5,8 +5,13 @@ for (product of products) {
     const [highlighted, allProds] = [product.querySelector('#currentHighlight'), product.querySelectorAll('.items-box__item-images--item')]
     //loop through inner Extra Image Collection block
     allProds.forEach(image => {
-        image.addEventListener('click', imageClicked =>
-        highlighted.setAttribute("style", `background-image: url(${imageClicked.target.src})`)
-        );
+        image.addEventListener('click', imageClicked => {
+            allProds.forEach(element => (element.style.opacity = 1)); //resets the opacity when toggle
+            highlighted.setAttribute("style", `background-image: url(${imageClicked.target.src})`);
+            highlighted.classList.add('fade-in');
+            // reset classname for highlighted element
+            setTimeout(() => highlighted.classList.remove('fade-in'), 500);
+            imageClicked.target.style.opacity = opacity;
+        });
     });
 }
