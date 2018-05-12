ec0a3e24f92916ee2f2931d6612986bd058224ae
Juozas Rastenis <aneke1234@gmail.com>
2018-05-12 20:25:41 +0300

    small fixes saturday

--- public/js/manual/googleMaps.js
+++ public/js/manual/googleMaps.js
@@ -11,12 +11,14 @@ function initMap() {
         });
     var marker = new google
         .maps
-        .Marker({position: uluru, map: map, animation: google.maps.Animation.BOUNCE});
+        .Marker({position: uluru, map: map, animation: google.maps.Animation.DROP});
     var infowindow = new google
         .maps
         .InfoWindow({content: contentString});
     infowindow.open(map, marker);
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
 }
-var contentString = '<div id="content"><div id="siteNotice"></div><h4 id="firstHeading" class="firstH' +
-        'eading">Lyra store</h4><div id="bodyContent"><p></p><p>Attribution: Uluru(last v' +
-        'isited June 22, 2009).</p></div></div>';
+var contentString = '<div id="content"><h4 id="firstHeading" class="firstH' +
+        'eading">Salonas Lyra</h4><div id="bodyContent"><p>Kęstučio g. 26, Vilnius</p><p>Tel.: (+370 5) 262 35 96</p><p></p></div>I - V - 10:00 - 19:00 val. VI - 10:00 - 15:00 val</div>';
