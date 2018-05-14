d9ab5f2eb96cce63ae415513037df7307a8e9a06
Juozas Rastenis <aneke1234@gmail.com>
2018-05-15 01:54:50 +0300

    Manufacturers styling done

--- public/js/manual/manufacturersList.js
+++ public/js/manual/manufacturersList.js
@@ -1,9 +1,8 @@
-import $ from 'jquery';
-
 // const proto = {
 //     init() {
 $('.clickable__block').click(function() {
     $(this).next('section').slideToggle('slow');
+    $('.dropdownSVG').toggleClass("rotate");
     if ($(this).next('section').is(':visible')) {
         $('html, body').animate({scrollTop: $(this).position().top}, 700);
     }
