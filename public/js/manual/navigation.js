e4dd2f5ea2b50e07bbb5e6b96f4dced509cab77b
Juozas Rastenis <aneke1234@gmail.com>
2018-06-20 14:59:14 +0300

    Started to clean up stuff + Sticky category navigation added

--- public/js/manual/navigation.js
+++ public/js/manual/navigation.js
@@ -1,7 +1,23 @@
-$('.navigation-dropdown').click(function() {
-    $(this).next('section').slideToggle('slow');
-    $('.dropdownSVG').toggleClass("rotate");
-    // if ($(this).next('section').is(':visible')) {
-    //     $('html, body').animate({scrollTop: $(this).position().top}, 700);
-    // }
+$('.navigation-dropdown').click(function () {
+	$(this).next('section').slideToggle('slow');
+	$('.dropdownSVG').toggleClass("rotate");
+	// if ($(this).next('section').is(':visible')) {
+	//     $('html, body').animate({scrollTop: $(this).position().top}, 700);
+	// }
+});
+
+const el = document.querySelector('.side-nav__box');
+const elTop = el.getBoundingClientRect().top - document.body.getBoundingClientRect().top - 60;
+const mq = window.matchMedia("(max-width: 700px)");
+console.log(mq);
+
+window.addEventListener('scroll', function () {
+    if (mq) {
+        if (document.documentElement.scrollTop > elTop) {
+            el.style.position = 'sticky';
+            el.style.top = '60px';
+        } else {
+            el.style.top = 'auto';
+        }
+    }
 });
