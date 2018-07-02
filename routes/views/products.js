49e9abec6b3db75ed655f0336e3850f628e66f47
Juozas Rastenis <aneke1234@gmail.com>
2018-07-02 22:53:20 +0300

    Got rid of metadata that comes along with products querying

--- routes/views/products.js
+++ routes/views/products.js
@@ -1,105 +1,117 @@
 let keystone = require('keystone');
-var async = require('async');
+let async = require('async');
+let mongoose = require('mongoose');
 
+exports = module.exports = function (req, res) {
+	let view = new keystone.View(req, res);
+	let locals = res.locals;
 
-exports = module.exports = function(req, res) {
-    let view = new keystone.View(req, res);
-    let locals = res.locals;
+	locals.section = 'products';
 
-    locals.section = 'products';
+	locals.data = {
+		products: [],
+		categories: [],
+		sort: req.query.filterlist
+	};
 
-    locals.data = {
-        products: [],
-        categories: [],
-        sort: req.query.filterlist
-    };
-
-    locals.filters = {
-        category: req.params.category,
-    };
+	locals.filters = {
+		category: req.params.category,
+	};
 
 	// Load all categories for side navigation
 	view.on('init', function (next) {
 		keystone.list('ProductCategory').model.find().sort('name').exec(function (err, results) {
 			if (err || !results.length) {
 				return next(err);
-            }
+			}
 
-            locals.data.categories = results;
+			locals.data.categories = results;
 			//Load the counts for each category (counts how much products every category contains)
 			async.each(locals.data.categories, function (category, next) {
 				keystone.list('Product').model.count().where('ProductType').in([category.id]).exec(function (err, count) {
-                    category.postCount = count;
+					category.postCount = count;
 					next(err);
 				});
 			}, function (err) {
 				next(err);
 			});
 		});
-    });
+	});
 
-    // Load the current category Object
-    view.on('init', function (next) {
-    		if (req.params.category) {
-			keystone.list('ProductCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
-                locals.data.category = result;
+	// Load the current category Object
+	view.on('init', function (next) {
+		if (req.params.category) {
+			keystone.list('ProductCategory').model.findOne({
+				key: locals.filters.category
+			}).exec(function (err, result) {
+				locals.data.category = result;
 				next(err);
 			});
 		} else {
 			next();
 		}
-    });
+	});
 
-    // Load the products
-    view.on('init', function (next) {
+	// Load the products
+	view.on('init', function (next) {
 		let q = keystone.list('Product').paginate({
 			page: req.query.page || 1,
 			perPage: 9,
 			maxPages: 10,
-        })
-        q.populate('Manufacturer ProductType').sort(getSort());
+		})
+		q.populate('Manufacturer ProductType').sort(getSort());
 
 		if (locals.data.category) {
-            q.where('ProductType').in([locals.data.category]).sort(getSort());
-        }
+			q.where('ProductType').in([locals.data.category]).sort(getSort());
+		}
 
-        function getSort() {
-            if (req.query.filterlist == "price-high") {
-                return {'price': -1};
-            } else if (req.query.filterlist == "price-low") {
-                return {'price': 1};
-            }
-        }
+		function getSort() {
+			if (req.query.filterlist == "price-high") {
+				return {
+					'price': -1
+				};
+			} else if (req.query.filterlist == "price-low") {
+				return {
+					'price': 1
+				};
+			}
+		}
 
-        if(req.query.search) {
-            const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-            q.find({
-                $or: [
-                    {'slug': regex},
-                    {'title': regex},
-                ]
-            }, function(err, results) {
-                if(err) {
-                    next(err);
-                } else {
-                    locals.data.products.results = results;
-                    next(err);
-                }
-            })
-        } else {
-            q.exec(function (err, results) {
-                locals.data.products = results;
-                next(err);
-            })
-        }
+		if (req.query.search) {
+			const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+			q.find({
+				$or: [{
+						'slug': regex
+					},
+					{
+						'title': regex
+					},
+				]
+			}, function (err, results) {
+				if (err) {
+					next(err);
+				} else {
+					locals.data.products.results = results;
+					next(err);
+				}
+			})
+		} else {
+			q.exec(function (err, results) {
+				// getting rid of metadata with "toObject()" from mongoose
+				let result = results.results;
+				result.forEach(r => {
+					locals.data.products.push(r.toObject());
+				});
+				next(err);
+			})
+		}
 	});
-    // Additionally query manufacturers for section
-    view.query('manufacturers', keystone.list('ProductManufacturer').model.find());
-    // Render the view
+	// Additionally query manufacturers for section
+	view.query('manufacturers', keystone.list('ProductManufacturer').model.find());
+	// Render the view
 	view.render('products');
 };
 
 function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
-
