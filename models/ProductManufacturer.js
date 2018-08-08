4285a27090a79213e2dd27d57b4e148ee4b0759c
Juozas Rastenis <aneke1234@gmail.com>
2018-08-08 13:14:51 +0300

    Manufacturer model fields added

--- models/ProductManufacturer.js
+++ models/ProductManufacturer.js
@@ -2,28 +2,34 @@ var keystone = require('keystone');
 let Types = keystone.Field.Types;
 
 var ProductManufacturer = new keystone.List('ProductManufacturer', {
-	autokey: {
-		from: 'name',
-		path: 'key',
-		unique: true
-	},
+    autokey: {
+        from: 'name',
+        path: 'key',
+        unique: true
+    },
 });
 
 ProductManufacturer.add({
-	name: {
-		type: String,
-		required: true
-	},
-	image: {
-		type: Types.CloudinaryImage
-	},
+    name: {
+        type: String,
+        required: true
+    },
+    Logo: {
+        type: Types.CloudinaryImage
+    },
+    SmallPreviewImage: {
+        type: Types.CloudinaryImage
+    },
+    CoverImage: {
+        type: Types.CloudinaryImage
+    },
 });
 
 // ProductManufacturer.relationship({ ref: 'Post', path: 'posts', refPath: 'categories' });
 ProductManufacturer.relationship({
-	ref: 'Product',
-	path: 'products',
-	refPath: 'Manufacturer'
+    ref: 'Product',
+    path: 'products',
+    refPath: 'Manufacturer'
 });
 
 ProductManufacturer.register();
