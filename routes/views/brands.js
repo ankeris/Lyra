146e90c56aca1e42818215c51d6723a5ed0ef0bd
Juozas Rastenis <aneke1234@gmail.com>
2018-08-10 12:33:29 +0300

    new manufacturer images

--- routes/views/brands.js
+++ routes/views/brands.js
@@ -2,11 +2,11 @@ var keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
+    var view = new keystone.View(req, res);
     var locals = res.locals;
 
-    view.query('manufacturers', keystone.list('ProductManufacturer').model.find());
+    view.query('manufacturers', keystone.list('ProductManufacturer').model.find().sort('name'));
 
-	// Render the view
-	view.render('brands');
+    // Render the view
+    view.render('brands');
 };
