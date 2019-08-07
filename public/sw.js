d80547b4900df195eedbf9b3a5476fd385226b04
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-08-07 19:32:08 +0200

    simple service worker added

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log('workbox is working');
+
+
+    /* Cache Font Files */
+    workbox.routing.registerRoute(
+        new RegExp(/(woff|woff2|ttf|otf)((\?.*)$|$)/),
+        new workbox.strategies.CacheFirst({
+            cacheName: "static-fonts"
+        })
+    );
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
