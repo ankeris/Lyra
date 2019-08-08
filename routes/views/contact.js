86d2c91cd6755c74dc6fca5420bd4ef477d020e0
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-08-08 22:23:08 +0200

    Contact page with CMS

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,25 +1,55 @@
-var keystone = require('keystone');
-var Enquiry = keystone.list('Enquiry');
-const {isWebP} = require('../helpers');
+const keystone = require('keystone');
+const Enquiry = keystone.list('Enquiry');
+const { isWebP, cropCloudlinaryImage } = require('../helpers');
+const { findOneByKey } = require('../redis-queries/redisQueries');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+	const view = new keystone.View(req, res);
+	let locals = res.locals;
 
 	// Set locals
 	locals.supportWebP = isWebP(req);
 	locals.section = 'contact';
+	locals.data = {
+		description: '',
+		images: []
+	},
+		locals.validationErrors = {};
 	locals.formData = req.body || {};
-	locals.validationErrors = {};
 	locals.enquirySubmitted = false;
+
+	// Get text for "Contacts" page
+	view.on('init', function (next) {
+		findOneByKey({
+			dbCollection: keystone.list('Texts'),
+			keyName: 'contact-page-text',
+			callback: (result, err) => {
+				locals.data.description = result.Text;
+				next(err);
+			}
+		});
+	});
+
+	// Get Images for "Contacts" page
+	view.on('init', function (next) {
+		findOneByKey({
+			dbCollection: keystone.list('Images'),
+			keyName: 'contact-page-switching-images',
+			callback: (result, err) => {
+				const images = result.Images.map(image => cropCloudlinaryImage(image, 1000, 1000, locals.supportWebP));
+				locals.data.images = images;
+				next(err);
+			}
+		});
+	});
 
 	// On POST requests, add the Enquiry item to the database
 	view.on('post', { action: 'contact' }, function (next) {
 
 		var newEnquiry = new Enquiry.model();
 		var updater = newEnquiry.getUpdateHandler(req);
-		
+
 		// console.log(locals.formData);
 
 		updater.process(req.body, {
