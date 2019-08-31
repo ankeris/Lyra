dffeafed292ef06791c4c5acd211c6743127c452
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-08-31 12:49:12 +0200

    manifest intro appearance effects added

--- public/js/manual/index/manifesto.js
+++ public/js/manual/index/manifesto.js
@@ -1,10 +1,18 @@
-$ = require('jquery');
-let allSpans = document.querySelectorAll('.manifesto .message--huge span');
-
-$(document).ready(function() {
-	for (let i = 0; i < allSpans.length; i++) {
-		setTimeout(function() {
-			allSpans[i].className = 'fade-in';
-		}, i * 700);
-	}
-});
+const wordsWrapper = document.querySelector('.manifesto .message--huge');
+const allSpans = document.querySelectorAll('.manifesto .message--huge span');
+const logo = document.getElementById('logoDissapear');
+document.addEventListener('DOMContentLoaded', function() {
+	setTimeout(() => {
+		logo.classList.add('fade-out');
+		setTimeout(() => {
+			logo.classList.add('hidden');
+			wordsWrapper.classList.remove('hidden');
+			wordsWrapper.classList.add('fade-in');
+			for (let i = 0; i < allSpans.length; i++) {
+				setTimeout(function() {
+					allSpans[i].className = 'fade-in';
+				}, i * 300);
+			}
+		}, 400);
+	}, 1500);
+}, false);
--- public/js/manual/product/cableLogic.js
+++ public/js/manual/product/cableLogic.js
@@ -1,6 +1,8 @@
 const selector = document.getElementById('cablePriceSelect');
 const displayer = document.getElementById('cablePrice');
 
-selector.addEventListener('change', (ev) => {
-	displayer.innerHTML = `${ev.target.value} €`;
-});
+if (selector && displayer) {
+	selector.addEventListener('change', (ev) => {
+		displayer.innerHTML = `${ev.target.value} €`;
+	});
+}
